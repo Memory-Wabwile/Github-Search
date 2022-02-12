8eaa7c15d040034bf149c38d221733fdbaa72b33
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-12 11:26:46 +0300

    generate a http service

---
+++ src/app/http-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+
+  constructor(private http:HttpClient) { }
+
+  getData():Observable<any>
+{
+  return this.http.get<any>(URL)
+}
+}
