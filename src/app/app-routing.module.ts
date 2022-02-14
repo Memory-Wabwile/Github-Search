e03dc8d75c837d270722123233d1641a2d8ea7e7
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-14 07:57:41 +0300

    add the routing to the navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { GithubReposComponent } from './github-repos/github-repos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'user-profile' , component : UserProfileComponent},
+  {path : 'github-repos' , component : GithubReposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
